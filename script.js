e96bb26c7260e027253e8448d228ceb1e21b0eab
hammad02520 <hammadsmit@gmail.com>
2024-08-11 19:18:56 +0500

    Fir commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Declaring 3 variables in one statement
+var age = 40, birth = "26", number = 1234567890;
+console.log(age, birth, number); 
+
+// 5 Legal Variable Names
+var firstName;
+
+var _age;
+var $salary;
+var birthYear;
+var isActive;
+
+console.log(firstName, _age, $salary, birthYear, isActive); 
+
+// Displaying the rules in the browser
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("Variable names can only contain <strong>letters</strong>, <strong>numbers</strong>, <strong>underscore (_)</strong>, and <strong>dollar sign ($)</strong>. For example <code>$my_1stVariable</code><br>");
+document.write("Variables must begin with a <strong>letter</strong>, <strong>underscore (_)</strong> or <strong>dollar sign ($)</strong>. For example <code>$name</code>, <code>_name</code>, or <code>name</code><br>");
+document.write("Variable names are case <strong>sensitive</strong>.<br>");
+document.write("Variable names should not be JS <strong>keywords</strong>.<br>");
